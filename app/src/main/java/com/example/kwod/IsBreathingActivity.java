9546f4fced471a015a129be87c222b7fa2610f47
Michał Jan Nurek <m.nurek@stud.elka.pw.edu.pl>
2022-06-08 20:46:50 +0200

    IsBreathingActivity, next and back buttons working fine

--- app/src/main/java/com/example/kwod/IsBreathingActivity.java
+++ app/src/main/java/com/example/kwod/IsBreathingActivity.java
@@ -1,6 +1,7 @@
 package com.example.kwod;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,10 +11,15 @@ import android.widget.Button;
 import com.example.kwod.fragment.IsBreathingCallFragment;
 import com.example.kwod.fragment.IsBreathingRecoveryPositionFragment;
 
+import java.util.ArrayList;
+
 public class IsBreathingActivity extends AppCompatActivity {
 
     private Button isBreathingBackButton;
     private Button isBreathingNextButton;
+
+    private ArrayList<Fragment> isBreathingFragmentList = new ArrayList<>();
+    private Integer position = 0;
 
     public static final String IS_BREATHING_MESSAGE = "IS_BREATHING_MESSAGE";
     @Override
@@ -21,21 +27,33 @@ public class IsBreathingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_breathing);
 
+        isBreathingFragmentList.add(new IsBreathingCallFragment());
+        isBreathingFragmentList.add(new IsBreathingRecoveryPositionFragment());
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
                 .replace(R.id.isBreathingFragment, new IsBreathingCallFragment())
                 .commit();
 
-
         isBreathingBackButton = findViewById(R.id.isBreathingBackButton);
         isBreathingNextButton = findViewById(R.id.isBreathingNextButton);
 
-        isBreathingNextButton.setOnClickListener(v -> {
+        isBreathingBackButton.setOnClickListener(v -> {
+            position = Math.abs((position - 1) % isBreathingFragmentList.size());
             getSupportFragmentManager()
                     .beginTransaction()
                     .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                    .replace(R.id.isBreathingFragment, new IsBreathingRecoveryPositionFragment())
+                    .replace(R.id.isBreathingFragment, isBreathingFragmentList.get(position))
+                    .commit();
+        });
+        isBreathingNextButton.setOnClickListener(v -> {
+            position = Math.abs((position + 1) % isBreathingFragmentList.size());
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                    .replace(R.id.isBreathingFragment, isBreathingFragmentList.get(position))
                     .commit();
         });
     }
