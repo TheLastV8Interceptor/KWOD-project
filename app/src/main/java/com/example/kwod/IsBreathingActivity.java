9a5041347048b47234c16e85a002b4461afea5b0
Michał Jan Nurek <m.nurek@stud.elka.pw.edu.pl>
2022-06-08 11:05:34 +0200

    Fragments for IsBreathingActivity created - IsBreathingCallFragment and IsBreathingRecoveryPositionFragment + instructions for emergency call and for recovery position

--- app/src/main/java/com/example/kwod/IsBreathingActivity.java
+++ app/src/main/java/com/example/kwod/IsBreathingActivity.java
@@ -5,14 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.kwod.fragment.IsBreathingCallFragment;
+import com.example.kwod.fragment.IsBreathingRecoveryPositionFragment;
 
 public class IsBreathingActivity extends AppCompatActivity {
+
+    private Button isBreathingBackButton;
+    private Button isBreathingNextButton;
 
     public static final String IS_BREATHING_MESSAGE = "IS_BREATHING_MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_breathing);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                .replace(R.id.isBreathingFragment, new IsBreathingCallFragment())
+                .commit();
+
+
+        isBreathingBackButton = findViewById(R.id.isBreathingBackButton);
+        isBreathingNextButton = findViewById(R.id.isBreathingNextButton);
+
+        isBreathingNextButton.setOnClickListener(v -> {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                    .replace(R.id.isBreathingFragment, new IsBreathingRecoveryPositionFragment())
+                    .commit();
+        });
     }
 
     public void switch_to_EndHelpActivity(View view)
